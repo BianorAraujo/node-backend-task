344b24539f7378c4cd678d1db30fa39e91eb6de1
Bianor Araujo <bianor.araujo@gmail.com>
2020-09-13 02:06:34 -0300

    adiconar db produção e configurar variáveis de ambiente para o heroku

--- knexfile.js
+++ knexfile.js
@@ -2,18 +2,31 @@
 
 module.exports = {
 
-  client: 'postgresql',
-  connection: {
-    database: 'tasks',
-    user:     'postgres',
-    password: 'Postgre123'
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'tasks',
+      user:     'postgres',
+      password: 'Postgre123'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 
 };
